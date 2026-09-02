089588e247407f942fb5ac794e382f1ecbd8b978
juanjo-rs <juan.rios62744@ucaldas.edu.co>
2026-09-02 17:30:47 -0500

    feat

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+console.log("Accedí al JS");
+function saludoFuncion(){
+    console.log(" Hola desde la funcion")
+}
+const SALUDO_FLECHA = () => {
+    console.log("hola desde la funcion flecha")
+}
+const SALUDO_DOS = function() {
+    console.log("hola desde la funcion anonima")
+}
